a908377c29cc3db2fefa3f11f60e2c6186142bf3
平川祥太 <shota@hirakawashoutanoMacBook-Pro.local>
2021-05-24 21:53:50 +0900

    line_itemとcartを定義

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -24,3 +24,11 @@ Route::name('product.')->group(function() {
   Route::get('/', 'ProductController@index')->name('index');
   Route::get('/product/{id}', 'ProductController@show')->name('show');
 });
+
+Route::name('line_item.')->group(function() {
+  Route::post('/line_item/create', 'LineItemController@create')->name('create');
+});
+
+Route::name('cart.')->group(function() {
+  Route::get('/cart', 'CartController@index')->name('index');
+});
